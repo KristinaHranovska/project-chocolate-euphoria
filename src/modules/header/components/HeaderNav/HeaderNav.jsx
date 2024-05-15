deac2da95efb50bdc3fea5b2547dd5689cb3b407
Kristina <kr.hranovska@gmail.com>
2024-05-15 21:34:46 +0300

    add animation burger-menu

--- src/modules/header/components/HeaderNav/HeaderNav.jsx
+++ src/modules/header/components/HeaderNav/HeaderNav.jsx
@@ -1,24 +1,57 @@
+import { useState } from 'react';
+import { Transition } from 'react-transition-group';
+
+import { icons as sprite } from 'shared/icons';
 import style from './HeaderNav.module.scss';
 
 const HeaderNav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <ul className={style.navList}>
-      <li>
-        <a className={style.navListItem} href="">
-          How it’s made?
-        </a>
-      </li>
-      <li>
-        <a className={style.navListItem} href="">
-          Our products
-        </a>
-      </li>
-      <li>
-        <a className={style.navListItem} href="">
-          Chocolate is loved
-        </a>
-      </li>
-    </ul>
+    <>
+      <div className={style.thumbMenu} onClick={toggleMenu}>
+        <svg className={style.mobileMenu}>
+          <use xlinkHref={`${sprite}#burger-menu`} />
+        </svg>
+      </div>
+
+      <Transition in={isMenuOpen} timeout={300}>
+        {(state) => (
+          <>
+            {state === 'entered' && (
+              <div className={style.backdrop} onClick={toggleMenu} />
+            )}
+            <div
+              className={`${style.overlayMenu} ${
+                state === 'entered' ? style.entered : ''
+              }`}
+            />
+          </>
+        )}
+      </Transition>
+
+      <ul className={style.navList}>
+        <li>
+          <a className={style.navListItem} href="">
+            How it’s made?
+          </a>
+        </li>
+        <li>
+          <a className={style.navListItem} href="">
+            Our products
+          </a>
+        </li>
+        <li>
+          <a className={style.navListItem} href="">
+            Chocolate is loved
+          </a>
+        </li>
+      </ul>
+    </>
   );
 };
 

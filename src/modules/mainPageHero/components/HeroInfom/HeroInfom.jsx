92e27a1594b4deaf9b3b81981f67d965a590f0f4
Kristina <kr.hranovska@gmail.com>
2024-05-19 17:45:41 +0300

    hero is ready

--- src/modules/mainPageHero/components/HeroInfom/HeroInfom.jsx
+++ src/modules/mainPageHero/components/HeroInfom/HeroInfom.jsx
@@ -1,25 +1,16 @@
 import { Button } from 'shared/components';
 import style from './HeroInfom.module.scss';
-import { icons as sprite } from 'shared/icons';
 
 const HeroInfom = () => {
   return (
     <div className={style.heroInform}>
-      <h1>
+      <h1 className={style.heroMainTitle}>
         Treat yourself or a loved one to our finest ingredients for a moment of
         pure delight!
       </h1>
       <div className={style.heroBtn}>
         <Button title={'Buy now'} className={style.btnOrangeStyle} />
         <Button title={'How it’s made'} border={true} />
-      </div>
-      <div className={style.scrollStyle}>
-        <p>scroll down</p>
-        <Button className={style.btnArrowDown}>
-          <svg className={style.iconHeroArrow}>
-            <use xlinkHref={`${sprite}#arrow-down`} />
-          </svg>
-        </Button>
       </div>
     </div>
   );

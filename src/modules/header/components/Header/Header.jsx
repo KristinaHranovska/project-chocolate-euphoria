32c40709936d9399758e1298ecdc010c54ef26be
Kristina <kr.hranovska@gmail.com>
2024-05-19 22:08:31 +0300

    some changes

--- src/modules/header/components/Header/Header.jsx
+++ src/modules/header/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import { useMediaQuery } from 'react-responsive';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 import { Logo } from 'shared/components';
 import HeaderNav from '../HeaderNav/HeaderNav';
@@ -7,9 +9,31 @@ import style from './Header.module.scss';
 
 const Header = () => {
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1439px)' });
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <header className={style.header} id="home">
+    <header
+      className={clsx(style.header, {
+        [style.headerContainer]: isTabletOrMobile && scroll,
+      })}
+      id="home"
+    >
       {isTabletOrMobile && <Logo className={style.logo} />}
       <HeaderNav />
       <BasketButton />

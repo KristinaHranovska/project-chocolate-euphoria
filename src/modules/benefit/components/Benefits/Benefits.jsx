8f781986c78b32d563af5b0ff86de3f4e4e18c34
Kristina <kr.hranovska@gmail.com>
2024-05-19 20:52:02 +0300

    benefits is ready

--- src/modules/benefit/components/Benefits/Benefits.jsx
+++ src/modules/benefit/components/Benefits/Benefits.jsx
@@ -1,13 +1,24 @@
+import { useMediaQuery } from 'react-responsive';
+
 import benefits from 'modules/benefit/helpers/benefits';
 import { Container } from 'shared/components';
 import style from './Benefits.module.scss';
 import { icons as sprite } from 'shared/icons';
+import { imagesDesktop2x } from 'shared/images';
 
 const Benefits = () => {
+  const isTabletOrDesktop = useMediaQuery({ query: '(min-width: 768px)' });
   return (
     <section className={style.benefitsSection}>
       <h2 className={style.visuallyHidden}>Benefits</h2>
       <div className={style.benefitsThumb}>
+        {isTabletOrDesktop && (
+          <img
+            className={style.benefitsImgUp}
+            src={imagesDesktop2x.lineUp2x}
+            alt="line"
+          />
+        )}
         <Container>
           <ul className={style.benefitsList}>
             {benefits.map((benefit, index) => (
@@ -25,6 +36,13 @@ const Benefits = () => {
             ))}
           </ul>
         </Container>
+        {isTabletOrDesktop && (
+          <img
+            className={style.benefitsImgDown}
+            src={imagesDesktop2x.lineDown2x}
+            alt="line"
+          />
+        )}
       </div>
     </section>
   );

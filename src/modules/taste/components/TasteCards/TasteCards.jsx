3e73a8228c3bd00d93e646f415684ad099565626
Kristina <kr.hranovska@gmail.com>
2024-05-20 22:22:03 +0300

    taste section mobile is ready

--- src/modules/taste/components/TasteCards/TasteCards.jsx
+++ src/modules/taste/components/TasteCards/TasteCards.jsx
@@ -1,7 +1,66 @@
-// import style from './TasteCards.module.scss';
+import tasteDescription from 'modules/taste/helpers/tasteDescription';
+import style from './TasteCards.module.scss';
+import { icons as sprite } from 'shared/icons';
+import { useState } from 'react';
 
 const TasteCards = () => {
-  return <></>;
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleOverlayClick = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+  return (
+    <>
+      <ul className={style.tasteList}>
+        {tasteDescription.map((taste, index) => (
+          <li
+            key={index}
+            className={`${style.tasteItem} ${index % 2 === 0 ? style.odd : style.even}`}
+          >
+            <div className={style.thumbImage}>
+              <img className={style.imgTaste} src={taste.image} alt="taste" />
+              <div className={style.imgBlock}></div>
+            </div>
+            <div
+              className={`${style.tasteBlock} ${index % 2 !== 0 && style.even}`}
+            >
+              <h3 className={style.tasteTitle}>{taste.title}</h3>
+              <div className={style.thumbIconDote}>
+                <svg className={style.doteIcon}>
+                  <use xlinkHref={`${sprite}#dote`} />
+                </svg>
+              </div>
+              <p className={style.textTaste}>{taste.text}</p>
+              <div
+                className={`${style.thumbIcon} ${style.onClickOverlay}`}
+                onClick={() => handleOverlayClick(index)}
+              >
+                <svg className={style.arrowIcon}>
+                  <use xlinkHref={`${sprite}#arrow-long-right`} />
+                </svg>
+              </div>
+            </div>
+            <div
+              className={`${style.overlay} ${activeIndex === index ? style.activeOverlay : ''}`}
+            >
+              <h3 className={style.tasteTitle}>{taste.title}</h3>
+              <p className={`${style.textTaste} ${style.resetMargin}`}>
+                {taste.description}
+              </p>
+              <div
+                className={`${style.thumbIcon} ${style.onClickOverlay}`}
+                onClick={() => handleOverlayClick(index)}
+              >
+                <svg className={style.arrowIconLeft}>
+                  <use xlinkHref={`${sprite}#arrow-long-left`} />
+                </svg>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default TasteCards;
---
+++ src/modules/taste/helpers/tasteDescription.js
@@ -0,0 +1,26 @@
+import {
+    imagesDesktop2x,
+} from 'shared/images';
+
+const tasteDescription = [
+    {
+        image: imagesDesktop2x.milkChocolate2x,
+        title: "milk chocolate",
+        text: "Crafted with the finest cocoa beans, fresh milk, our milk chocolate delivers a velvety smooth texture and a luscious flavor that will captivate your senses.",
+        description: "Milk chocolate is a solid chocolate confectionery containing cocoa, sugar and milk. It is the most consumed type of chocolate. Chocolate was originally sold and consumed as a beverage in pre-Columbian times. Although four-fifths of all milk chocolate is sold in the United States and Europe."
+    },
+    {
+        image: imagesDesktop2x.milk2x,
+        title: "milk",
+        text: "Sourced from the finest dairy farms and crafted without additives or artificial flavors, our milk offers a pure and invigorating taste experience.",
+        description: "Milk is a white liquid food produced by the mammary glands of mammals. It is the primary source of nutrition for young mammals (including breastfed human infants) before they are able to digest solid food.Early-lactation milk, which is called colostrum, contains antibodies that strengthen the immune system."
+    },
+    {
+        image: imagesDesktop2x.nuts2x,
+        title: "nuts",
+        text: "Embark on a journey of indulgence where creamy milk chocolate meets the rich essence of nuts.",
+        description: "Almonds, pistachios, and walnuts are some types of nuts that contain healthy nutrients. When eaten as part of a nutrient-dense diet, these 9 nuts may offer benefits such as reducing your risk of heart disease. They’re a good source of fiber, healthy fats, and plant protein. Plus, they’re great on their own."
+    }
+];
+
+export default tasteDescription;

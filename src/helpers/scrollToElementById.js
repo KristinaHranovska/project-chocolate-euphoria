25c1ccd6432087c784e222a0ab977d067ee91ad5
Kristina <kr.hranovska@gmail.com>
2024-05-24 17:04:48 +0300

    change scroll

--- src/helpers/scrollToElementById.js
+++ src/helpers/scrollToElementById.js
@@ -1,9 +1,7 @@
 export const scrollToElementById = (id) => {
     const getSectionById = document.querySelector(`#${id}`);
     if (!getSectionById) return;
-    const isMobileView = window.innerWidth < 768;
-    const isTabletView = window.innerWidth >= 768 && window.innerWidth < 1440;
-    const topOffset = isMobileView ? 56 : isTabletView ? 72 : 76;
+    const topOffset = 0;
     const topPosition = getSectionById.offsetTop - topOffset;
 
     window.scrollTo({

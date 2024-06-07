2864b88538f27f031a548c218e7f47d59b697dd4
Kristina <kr.hranovska@gmail.com>
2024-06-07 16:03:31 +0300

    scroll

--- src/shared/components/CustomScrollWrapper/CustomScrollWrapper.jsx
+++ src/shared/components/CustomScrollWrapper/CustomScrollWrapper.jsx
@@ -1,22 +1,17 @@
 import 'overlayscrollbars/styles/overlayscrollbars.css';
+import './CustomScrollWrapper.scss';
 import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import clsx from 'clsx';
 
 const CustomScrollWrapper = ({ children, wrapClassName }) => {
   return (
     <OverlayScrollbarsComponent
+      element="div"
       className={clsx('myScroll', wrapClassName && wrapClassName)}
       options={{
-        scrollbars: {
-          visibility: 'auto',
-          autoHide: 'leave',
-          autoHideDelay: 800,
-          dragScrolling: true,
-          clickScrolling: false,
-          touchSupport: true,
-          snapHandle: false,
-        },
+        scrollbars: { autoHide: 'never', theme: 'no-theme' },
       }}
+      defer
     >
       {children}
     </OverlayScrollbarsComponent>

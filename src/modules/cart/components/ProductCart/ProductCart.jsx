e3be23da2626eb1a885a23e88c0d26221b27dee4
Kristina <kr.hranovska@gmail.com>
2024-06-10 15:48:44 +0300

    cart is ready

--- src/modules/cart/components/ProductCart/ProductCart.jsx
+++ src/modules/cart/components/ProductCart/ProductCart.jsx
@@ -7,6 +7,7 @@ import {
 import style from './ProductCart.module.scss';
 import { icons as sprite } from 'shared/icons';
 import { useDispatch, useSelector } from 'react-redux';
+import CustomScrollWrapper from 'shared/components/CustomScrollWrapper/CustomScrollWrapper';
 
 const ProductCart = () => {
   const dispatch = useDispatch();
@@ -26,56 +27,63 @@ const ProductCart = () => {
 
   return (
     <>
-      <ul className={style.productCartList}>
-        {products.map((item) => {
-          const productTotalPrice =
-            parseFloat(item.product.price) * item.quantity;
-          return (
-            <li className={style.productCartItem} key={item.product._id}>
-              <img
-                className={style.productCartPhoto}
-                src={item.product.photo}
-                alt={item.product.productName}
-              />
-              <div className={style.productCartInfo}>
-                <p>{item.product.productName}</p>
-                <div className={style.quantity}>
+      <CustomScrollWrapper>
+        <ul className={style.productCartList}>
+          {products.map((item) => {
+            const productTotalPrice =
+              parseFloat(item.product.price) * item.quantity;
+            return (
+              <li className={style.productCartItem} key={item.product._id}>
+                <div className={style.productCartStyle}>
+                  <img
+                    className={style.productCartPhoto}
+                    src={item.product.photo}
+                    alt={item.product.productName}
+                  />
+                  <div className={style.productCartInfo}>
+                    <h3 className={style.productCartName}>
+                      {item.product.productName}
+                    </h3>
+                    <div className={style.quantity}>
+                      <button
+                        className={style.quantityBtn}
+                        type="button"
+                        onClick={() => handleDecrease(item.product._id)}
+                      >
+                        <svg className={style.iconQuantity}>
+                          <use xlinkHref={`${sprite}#minus`}></use>
+                        </svg>
+                      </button>
+                      <span className={style.quantityValue}>
+                        {item.quantity}
+                      </span>
+                      <button
+                        className={style.quantityBtn}
+                        type="button"
+                        onClick={() => handleIncrease(item.product._id)}
+                      >
+                        <svg className={style.iconQuantity}>
+                          <use xlinkHref={`${sprite}#plus`} />
+                        </svg>
+                      </button>
+                    </div>
+                    <p>Total: {productTotalPrice.toFixed(2)} UAH</p>
+                  </div>
                   <button
-                    className={style.quantityBtn}
                     type="button"
-                    onClick={() => handleDecrease(item.product._id)}
+                    className={style.productCartCan}
+                    onClick={() => handleDelete(item.product._id)}
                   >
-                    <svg className={style.iconQuantity}>
-                      <use xlinkHref={`${sprite}#minus`}></use>
-                    </svg>
-                  </button>
-                  <span className={style.quantityValue}>{item.quantity}</span>
-                  <button
-                    className={style.quantityBtn}
-                    type="button"
-                    onClick={() => handleIncrease(item.product._id)}
-                  >
-                    <svg className={style.iconQuantity}>
-                      <use xlinkHref={`${sprite}#plus`} />
+                    <svg className={style.iconCan}>
+                      <use xlinkHref={`${sprite}#can`}></use>
                     </svg>
                   </button>
                 </div>
-                <p>Total: {productTotalPrice.toFixed(2)} UAH</p>
-              </div>
-
-              <button
-                type="button"
-                className={style.productCartCan}
-                onClick={() => handleDelete(item.product._id)}
-              >
-                <svg className={style.iconCan} width="27" height="27">
-                  <use xlinkHref={`${sprite}#can`}></use>
-                </svg>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+              </li>
+            );
+          })}
+        </ul>
+      </CustomScrollWrapper>
     </>
   );
 };

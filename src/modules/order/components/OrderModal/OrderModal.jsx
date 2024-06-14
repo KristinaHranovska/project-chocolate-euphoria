6331754a288dc61fdb27aae02d32498154d1de79
Kristina <kr.hranovska@gmail.com>
2024-06-14 18:32:26 +0300

    tablet order is ready

--- src/modules/order/components/OrderModal/OrderModal.jsx
+++ src/modules/order/components/OrderModal/OrderModal.jsx
@@ -2,17 +2,20 @@ import style from './OrderModal.module.scss';
 
 import OrderProducts from '../OrderProducts/OrderProducts';
 import OrderForm from '../OrderForm/OrderForm';
+import CustomScrollWrapper from 'shared/components/CustomScrollWrapper/CustomScrollWrapper';
 
 const OrderModal = ({ closeModal }) => {
   return (
-    <div className={style.orderModal}>
-      <h2 className={style.orderTitle}>Complete Your Order</h2>
+    <CustomScrollWrapper>
+      <div className={style.orderModal}>
+        <h2 className={style.orderTitle}>Complete Your Order</h2>
 
-      <div className={style.orderInformation}>
-        <OrderForm closeModal={closeModal} />
-        <OrderProducts closeModal={closeModal} />
+        <div className={style.orderInformation}>
+          <OrderForm closeModal={closeModal} />
+          <OrderProducts closeModal={closeModal} />
+        </div>
       </div>
-    </div>
+    </CustomScrollWrapper>
   );
 };
 
